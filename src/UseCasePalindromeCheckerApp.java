367f326a325e95099a912e04b378890010139cd3
sathviksaisrinivas <marupudisathvik@gmail.com>
2026-03-05 13:00:25 +0530

    src/UseCasePalindromeCheckerApp.java

---
+++ src/UseCasePalindromeCheckerApp.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class UseCasePalindromeCheckerApp {
+
+    public static boolean isPalindrome(String str) {
+        String reverse = "";
+
+        for(int i=str.length()-1;i>=0;i--)
+            reverse += str.charAt(i);
+
+        return str.equals(reverse);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter text: ");
+        String text = sc.nextLine();
+
+        if(isPalindrome(text))
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+
+        sc.close();
+    }
+}
