39de4f59b4cc15abf39c23fa6ddc6ce718312325
sathviksaisrinivas <marupudisathvik@gmail.com>
2026-03-05 12:56:00 +0530

    ucl3 implementation

---
+++ src/UseCasePalindromeCheckerApp.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class UseCasePalindromeCheckerApp {
+
+    public static boolean isPalindrome(String str) {
+        str = str.replaceAll("\\s+", "").toLowerCase();
+        String reversed = new StringBuilder(str).reverse().toString();
+        return str.equals(reversed);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter text: ");
+        String text = sc.nextLine();
+
+        if (isPalindrome(text))
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+
+        sc.close();
+    }
+}
