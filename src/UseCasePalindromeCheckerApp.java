50a3e828de7e31d91b2d44e8c8d2be2a8fe58958
sathviksaisrinivas <marupudisathvik@gmail.com>
2026-03-05 13:06:34 +0530

    uc11 implemenattion
    Implement a palindrome checker that reads input from the user and checks if it is a palindrome.

---
+++ src/UseCasePalindromeCheckerApp.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class UseCasePalindromeCheckerApp {
+
+    public static boolean isPalindrome(String str) {
+
+        int start = 0;
+        int end = str.length() - 1;
+
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end))
+                return false;
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter text: ");
+        String text = sc.nextLine();
+
+        System.out.println(isPalindrome(text));
+
+        sc.close();
+    }
+}
